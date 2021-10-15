18cf2e42a44a0aee0096dbb943417c8123b17833
Mariana Costa <mariana.costa214@gmail.com>
2021-10-15 15:31:24 +0100

    Update and fetch high scores

--- src/server.js
+++ src/server.js
@@ -26,6 +26,23 @@ async function connectDb() {
 
 const db = await connectDb();
 
+async function updateHighScore(username, score) {
+    db.collection("players").updateOne(
+        { username },
+        { $set: { highScore: score } }
+    );
+}
+
+async function fetchHighScores() {
+    return db
+        .collection("players")
+        .find()
+        .project({ password: 0, _id: 0 })
+        .sort({ "highScore.score": 1, "highScore.timestamp": -1 })
+        .limit(10)
+        .toArray();
+}
+
 io.on("connection", (socket) => {
     console.log(`Socket ${socket.id} connected`);
 
@@ -38,14 +55,31 @@ io.on("connection", (socket) => {
         console.log(`Initialized game for socket ${socket.id}`);
     });
 
-    socket.on("play", ({ row, col, game }) => {
+    socket.on("play", async ({ row, col, game }) => {
         if (row !== null && col !== null && game) {
             console.log(
                 `Uncovering cell ${row} ${col} for socket ${socket.id}`
             );
             uncoverCell(row, col, game, true);
             updateGameStatus(game);
-            socket.emit("play", game);
+            if (game.status !== gameStatus.ONGOING) {
+                if (game.status === gameStatus.WON) {
+                    await updateHighScore("test2", {
+                        score: game.score,
+                        timestamp: Date.now(),
+                    });
+                }
+
+                let highScores = await fetchHighScores();
+                console.log(highScores);
+
+                socket.emit("end", {
+                    game,
+                    highScores,
+                });
+            } else {
+                socket.emit("play", game);
+            }
         }
     });
 
